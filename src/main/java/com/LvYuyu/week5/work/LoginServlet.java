488f083061446a2ff38297333206ca5f0d31bda0
lenovo <1434328725@qq.com>
2021-04-19 17:13:36 +0800

    7.HE Week7 Commit

---
+++ src/main/java/com/LvYuyu/week5/work/LoginServlet.java
@@ -0,0 +1,82 @@
+package com.LvYuyu.week5.work;
+
+import com.LvYuyu.dao.UserDao;
+import com.LvYuyu.model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    Connection con=null;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con=(Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String Username=request.getParameter("Username");
+        String password=request.getParameter("password");
+        UserDao userDao=new UserDao();
+        try {
+            User user=userDao.findByUsernamePassword(con,Username, password);
+            if(user!=null){
+                request.setAttribute("user",user);
+                request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
+            }else{
+                request.setAttribute("message","Username or password Error!!!");
+                request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        /*String sql="select * from Usertable where username=? and password=?";
+        PreparedStatement pstmt= null;
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,Username);
+            pstmt.setString(2,password);
+            ResultSet rs= pstmt.executeQuery();
+            PrintWriter out=response.getWriter();
+            if(rs.next()){
+                /*out.println("Login Success!!!");
+                out.println("Welcome,"+Username);*/
+                /*request.setAttribute("id",rs.getInt("id"));
+                request.setAttribute("username",rs.getString("username"));
+                request.setAttribute("password",rs.getString("password"));
+                request.setAttribute("email",rs.getString("email"));
+                request.setAttribute("gender",rs.getString("gender"));
+                request.setAttribute("birthdate",rs.getDate("birthdate"));
+                request.getRequestDispatcher("userInfo.jsp").forward(request,response);
+            }else {
+                //out.println("Username or password Error!!!");
+                request.setAttribute("message","Username or password Error!!!");
+                request.getRequestDispatcher("login.jsp").forward(request,response);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }*/
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lenovo
+  Date: 2021/4/19
+  Time: 16:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@include file="header.jsp"%>
+<h1>Welcome to my home page</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+<%@include file="footer.jsp"%>
