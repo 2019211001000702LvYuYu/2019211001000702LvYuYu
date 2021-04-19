d1b42424c6eab62cec10732285e1d14451b7af4b
lenovo <1434328725@qq.com>
2021-04-19 22:56:47 +0800

    7.HE Week7 Commit

--- src/main/java/com/LvYuyu/week5/work/LoginServlet.java
+++ src/main/java/com/LvYuyu/week5/work/LoginServlet.java
@@ -42,36 +42,9 @@ public class LoginServlet extends HttpServlet {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        /*String sql="select * from Usertable where username=? and password=?";
-        PreparedStatement pstmt= null;
-        try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1,Username);
-            pstmt.setString(2,password);
-            ResultSet rs= pstmt.executeQuery();
-            PrintWriter out=response.getWriter();
-            if(rs.next()){
-                /*out.println("Login Success!!!");
-                out.println("Welcome,"+Username);*/
-                /*request.setAttribute("id",rs.getInt("id"));
-                request.setAttribute("username",rs.getString("username"));
-                request.setAttribute("password",rs.getString("password"));
-                request.setAttribute("email",rs.getString("email"));
-                request.setAttribute("gender",rs.getString("gender"));
-                request.setAttribute("birthdate",rs.getDate("birthdate"));
-                request.getRequestDispatcher("userInfo.jsp").forward(request,response);
-            }else {
-                //out.println("Username or password Error!!!");
-                request.setAttribute("message","Username or password Error!!!");
-                request.getRequestDispatcher("login.jsp").forward(request,response);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }*/
-    }
 
     @Override
-    public void destroy() {
+    public void destroy(){
         super.destroy();
         try {
             con.close();
